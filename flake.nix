5bd9ebee9480c3c5d3fbb04f5cdc3a4be95e50f3
Carsten Ramke <carsten.ramke@gmail.com>
2025-12-15 10:00:53 +0100

    feat(dev): nix add flake initial

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "camera-optics dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.pnpm
+            pkgs.rustup            # install Rust toolchains as declared in rust-toolchain.toml if you have one
+            pkgs.cargo
+            pkgs.pkg-config
+            pkgs.openssl           # common native deps
+            pkgs.protobuf          # if tauri build needs it
+          ];
+          shellHook = ''
+            pnpm install --frozen-lockfile
+            export PS1="[nix] $PS1"
+          '';
+        };
+      });
+}
