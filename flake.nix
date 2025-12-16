18f0197c5089c2c73ea75f70935904e2ef0cec61
Carsten Ramke <carsten.ramke@gmail.com>
2025-12-16 10:37:45 +0100

    chore(dev): bump nix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "camera-optics dev env";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,8 +10,30 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        runtimePkgs = with pkgs; [
+          gtk3
+          gdk-pixbuf
+          pango
+          harfbuzz
+          cairo
+          webkitgtk_4_1
+          libsoup_3
+          libayatana-appindicator
+          glib
+          zlib
+          libglvnd
+          libcanberra-gtk3
+          mesa
+          xorg.libX11
+          wayland
+        ];
+        dataPkgs = with pkgs; [
+          libcanberra
+          gsettings-desktop-schemas
+          adwaita-icon-theme
+        ];
         shell = pkgs.mkShell {
-          buildInputs = [
+          buildInputs = runtimePkgs ++ dataPkgs ++ [
             pkgs.nodejs_20
             pkgs.pnpm
             pkgs.rustup            # install Rust toolchains as declared in rust-toolchain.toml if you have one
@@ -26,6 +48,11 @@
             pnpm install --frozen-lockfile
             pre-commit install
             export PS1="[nix] $PS1"
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimePkgs}:$LD_LIBRARY_PATH
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.adwaita-icon-theme}/share:${pkgs.libcanberra}/share:$XDG_DATA_DIRS
+            export GTK_PATH=${pkgs.libcanberra-gtk3}/lib/gtk-3.0
+            export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri # required for hardware accelleration. Will not work due to nix sandboxing.
+            export GBM_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri # required for hardware accelleration. Will not work due to nix sandboxing.
           '';
         };
       in {
