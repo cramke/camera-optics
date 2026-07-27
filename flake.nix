b8bd97c57597b0fc13edc1d47365ad22ccc4713f
Carten Ramke <carsten.ramke@gmail.com>
2026-07-27 22:06:27 +0200

    feat(nix): support macos (darwin) and linux

--- flake.nix
+++ flake.nix
@@ -10,7 +10,11 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        runtimePkgs = with pkgs; [
+        inherit (pkgs) lib;
+
+        # The GTK/WebKit stack Tauri links against on Linux. macOS uses the
+        # system WKWebView instead, so none of this is referenced there.
+        linuxRuntimePkgs = with pkgs; [
           gtk3
           gdk-pixbuf
           pango
@@ -27,33 +31,48 @@
           xorg.libX11
           wayland
         ];
-        dataPkgs = with pkgs; [
+
+        linuxDataPkgs = with pkgs; [
           libcanberra
           gsettings-desktop-schemas
           adwaita-icon-theme
         ];
+
+        darwinPkgs = with pkgs; [
+          libiconv      # rust links against this on darwin
+          apple-sdk_15  # tauri 2 needs a recent WebKit/WKWebView SDK
+        ];
+
+        commonPkgs = with pkgs; [
+          nodejs_20
+          pnpm
+          rustup         # provides the cargo/rustc shims
+          pre-commit
+          mdbook         # For building the documentation
+          pkg-config
+          openssl        # common native deps
+          protobuf       # if tauri build needs it
+        ];
+
+        linuxEnv = ''
+          export LD_LIBRARY_PATH=${lib.makeLibraryPath linuxRuntimePkgs}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.adwaita-icon-theme}/share:${pkgs.libcanberra}/share:$XDG_DATA_DIRS
+          export GTK_PATH=${pkgs.libcanberra-gtk3}/lib/gtk-3.0
+          # required for hardware accelleration. Will not work due to nix sandboxing.
+          # (mesa no longer has a separate `drivers` output; GBM backends live in pkgs.libgbm)
+          export LIBGL_DRIVERS_PATH=${pkgs.mesa}/lib/dri
+        '';
+
         shell = pkgs.mkShell {
-          buildInputs = runtimePkgs ++ dataPkgs ++ [
-            pkgs.nodejs_20
-            pkgs.pnpm
-            pkgs.rustup            # install Rust toolchains as declared in rust-toolchain.toml if you have one
-            pkgs.cargo
-            pkgs.pre-commit
-            pkgs.mdbook            # For building the documentation
-            pkgs.pkg-config
-            pkgs.openssl           # common native deps
-            pkgs.protobuf          # if tauri build needs it
-          ];
+          buildInputs = commonPkgs
+            ++ lib.optionals pkgs.stdenv.isLinux (linuxRuntimePkgs ++ linuxDataPkgs)
+            ++ lib.optionals pkgs.stdenv.isDarwin darwinPkgs;
+
           shellHook = ''
             pnpm install --frozen-lockfile
             pre-commit install
             export PS1="[nix] $PS1"
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimePkgs}:$LD_LIBRARY_PATH
-            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.adwaita-icon-theme}/share:${pkgs.libcanberra}/share:$XDG_DATA_DIRS
-            export GTK_PATH=${pkgs.libcanberra-gtk3}/lib/gtk-3.0
-            export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri # required for hardware accelleration. Will not work due to nix sandboxing.
-            export GBM_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri # required for hardware accelleration. Will not work due to nix sandboxing.
-          '';
+          '' + lib.optionalString pkgs.stdenv.isLinux linuxEnv;
         };
       in {
         devShells.default = shell;
